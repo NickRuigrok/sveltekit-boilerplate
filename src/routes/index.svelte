21826488ddbe9800b5a6c0b5337285ae96c02a77
Nick Ruigrok <nickruigrok@nicks-mbp.lan>
2021-09-20 22:40:57 +0200

    Initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,15 @@
+<script>
+  import '../app.css';
+</script>
+
+<svelte:head>
+  <title>SvelteKit Boilerplate</title>
+</svelte:head>
+
+<h1 class="welcome">Welcome to this SvelteKit Boilerplate</h1>
+
+<style lang="postcss">
+  .welcome {
+    @apply text-center text-2xl font-bold py-5
+  }
+</style>
